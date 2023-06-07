818cc55a3211c3c0c53c1e6300b70a8cf2d39708
Konradpina <123588475+Konradpina@users.noreply.github.com>
2023-06-07 16:40:54 +0200

    Add files via upload

---
+++ header.js
@@ -0,0 +1,7 @@
+
+hambuger = document.querySelector(".hambuger")
+        hambuger.onclick =function(){
+            var Navigationbar =document.querySelector(".Navigationbar");
+        Navigationbar.classList.toggle("active")
+        }
+
